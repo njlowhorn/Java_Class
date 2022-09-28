9b68a9b1afd545b0bea67ccc7efdda750c87d389
njlowhorn <75708236+njlowhorn@users.noreply.github.com>
2022-09-27 20:56:06 -0400

    Update and rename GamingComputerBuildIcomplete to GamingComputerBuild.java

--- Module3/ProgrammingAssignment/ChapterNineAssignment/GamingComputerBuild.java
+++ Module3/ProgrammingAssignment/ChapterNineAssignment/GamingComputerBuild.java
@@ -8,14 +8,14 @@ package gamingcomputerbuild;
 public class GamingComputerBuild {
 
     public static void main(String[] args) {
-        GamingComputer gc1 = new GamingComputer("Aurora", "Alienware");
+        GamingComputer gc1 = new GamingComputer("Aurora", ManufacturingCompany.Alienware);
         gc1.setProcessor("AMD Ryzen 5");
         gc1.setGraphicsCard("NVIDIA GeForce RTX 3050");
         gc1.setStorage(256);
         gc1.setRandomAccessMemory(16);
         System.out.println(gc1);
         
-        Desktop d1= new Desktop("Pavilion", "HP", true);
+        Desktop d1= new Desktop("Pavilion", ManufacturingCompany.HP, true);
         d1.setProcessor("AMD Ryzen 7");
         d1.setGraphicsCard("NVIDIA GeForce RTX 3060");
         d1.setRandomAccessMemory(8);
@@ -24,8 +24,8 @@ public class GamingComputerBuild {
         
         Laptop L1 = new Laptop("12th Gen Intel Core i5-12500H", "NVIDIA GeForce RTX 3050", 16, 512, 4);
         L1.setName("x14");
-        L1.setManufacturingCompany("Alienware");
+        L1.setManufacturingCompany(ManufacturingCompany.Alienware);
         System.out.println(L1);
-    }
+    }//end main
     
-}
+}//end GamingComputerBuildClass
