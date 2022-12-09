bb9ab9b768969f36c31c5e87d72321faf8137271
njlowhorn <75708236+njlowhorn@users.noreply.github.com>
2022-12-08 23:34:57 -0500

    Update and rename SumTask to SumTask.java

--- Module8/ProgrammingAssignment/ChapterThirtyTwoExercise/SumTask.java
+++ Module8/ProgrammingAssignment/ChapterThirtyTwoExercise/SumTask.java
@@ -1,13 +1,14 @@
 
 package listsum;
 
-import java.util.concurrent.RecursiveAction;
+import java.util.Arrays;
+import java.util.concurrent.RecursiveTask;
 
 /**
- *
+ * Class that finds the sum of a list
  * @author Nolan Lowhorn
  */
-public class SumTask extends RecursiveAction{
+public class SumTask extends RecursiveTask<Double>{
     private int threshold;
     private double[] list;
 
@@ -59,7 +60,11 @@ public class SumTask extends RecursiveAction{
         this.list = list;
     }//end setList
     
-    
+    /**
+     * Method that finds the sum of a list
+     * @param list double[] - A list of doubles
+     * @return double - The sum of the doubles
+     */
     public double findSum(double[] list){
         double sum = 0;
         for (double value : list) {
@@ -68,15 +73,25 @@ public class SumTask extends RecursiveAction{
         return sum;
     }//end findSum
 
+    /**
+     * Method that splits up the list into a specified number of parts, finds the sum of each, and calculates the total sum
+     * @return Double - The total sum
+     */
     @Override
-    protected void compute() {
+    protected Double compute() {
         SumTask st = new SumTask();
+        double totalSum = 0;
         if(list.length < threshold){
-            st.findSum(list);
+            totalSum = st.findSum(list);
         }//end if
         else{
-            
+            int splitNumber = list.length / threshold;
+            for(int i = 0; i < splitNumber; i++){
+                double[] splitList = Arrays.copyOfRange(list, i, (splitNumber * (i + 1)));
+                totalSum += st.findSum(splitList);
+            }//end for
         }//end else
+        return totalSum;
     }//end compute
     
 }//end SumTask class
